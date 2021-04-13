d6f0fb756d5bc91b3aebce2dca70c46d91acacf6
Netta Avidan <nettavidan19@gmail.com>
2021-04-13 13:42:10 +0300

    update

--- 7.1/api.js
+++ 7.1/api.js
@@ -1,14 +1,35 @@
-const request = require('request')
+//const request = require('request')
+//const axios = require('axios')
+const superagent = require('superagent')
 
+
+//request
 //const callQuete = (topic, callback) => {
     //const url = 'https://www.tronalddump.io/tag/'
     const url = 'https://dog.ceo/api/breeds/list'
 
-    request({url: url, json:true}, (error,response) =>{
-        if(error){
-            console.log("there is an error")
-        } else{
-            console.log(`${response.body.message} `)
-        }
-    })
+    // request({url: url, json:true}, (error,response) =>{
+    //     if(error){
+    //         console.log("there is an error")
+    //     } else{
+    //         console.log(`${response.body.message} `)
+    //     }
+    // })
 //}
+
+
+//axios
+// const getData = async () =>{
+//     const data = await axios.get(url)
+//     console.log(data.data)
+// }
+
+// getData()
+
+//superagent
+superagent.get(url)
+.then(response =>{
+console.log(response.body.message)
+}).catch(err=>{
+    console.log(err)
+})
